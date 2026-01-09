c258bbf10cf891edd69bfc80d523c699d8e39cd1
IBTISSAM E <ibtissam.bergach@usmba.ac.ma>
2026-01-09 21:29:04 +0100

    Construction d’une image applicative complète

---
+++ Projet2/src/main/java/fr/univ/tp/studentapi/StudentController.java
@@ -0,0 +1,23 @@
+package fr.univ.tp.studentapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    private final StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return studentRepository.findAll(); // récupère les données depuis MySQL
+    }
+}
