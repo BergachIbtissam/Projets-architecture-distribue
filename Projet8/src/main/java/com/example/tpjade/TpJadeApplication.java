ccea8ea4bca686809647cd6ba76757c287f60f07
IBTISSAM E <ibtissam.bergach@usmba.ac.ma>
2026-01-09 21:35:18 +0100

    Cloud et Déploiement

---
+++ Projet8/src/main/java/com/example/tpjade/TpJadeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpjade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpJadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpJadeApplication.class, args);
+    }
+
+}
